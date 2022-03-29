91ab8b1114dd5b0910b16ca6d5e8d525a3613e11
Andres Fridlmeier <andresfrid@gmail.com>
2022-03-29 19:24:13 -0300

    Clase Promise, asinc y map

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,14 @@
-
+import React,{ useState, useEffect } from 'react';
 import './Card.css';
 import ItemCount from '../NavBar/ItemCount/ItemCount';
 
 
-export default function Card({data}){  
-    const {title, price, talle, stock} = data
-   
-    
-    
 
+export default function Card({data}){  
+    const {title, talle, price, stock, image} = data
     return(
         <div className="card-item">
+        <img className= "img-card" src={`./${image}`} alt={image} />
             <h2>{title}</h2>
             <p>Precio: ${price}</p>
             <p>Talle: {talle}</p>
--- src/components/ListProducts/ItemListContainer.js
+++ src/components/ListProducts/ItemListContainer.js
@@ -1,20 +1,65 @@
+import React,{useState, useEffect} from 'react';
 import Card from '../Card/Card.js';
 import './ItemListContainer.css';
 
-const ItemListContainer = ({children}) => {
-    let dataProduct = {
-        title: 'remera',
-        talle: 'XL',
-        price: 1500,
-        stock: 5
-    }
-    return(
-        <div className="container-list">
-            <div className="container-cards">
-                <h2>{children}</h2>
-                <Card data={dataProduct}/>
+const ItemListContainer = ({}) => { 
+    const mockProducts = [
+        {
+            id: 1,
+            title: 'remera',
+            talle: 'XL',
+            price: 1500,  
+            image: 'remera.jpg',
+            stock: 5
+        },
+        {
+            id: 2,
+            title: 'Jean',
+            talle: 'L',
+            price: 500, 
+            image: 'logo192',
+            stock: 10 
+        },
+        {
+            id: 3,
+            title: 'buzo',
+            talle: 'M',
+            price: 4000, 
+            image: 'buzo.jpg',
+            stock: 8 
+        }]
+    
+        const [products, setProducts] = useState([]) 
+        const getProducts = () => { 
+            return new Promise ((resolve, reject) => {
+                setTimeout( () => {
+                    return resolve (mockProducts)
+                }, 200);
+            })
+        }
+        useEffect( () => {
+            getProducts().then((data) => {
+                setProducts(data)
+            }).finally( () => {
+                console.log("Termino la llamada")
+            })
+
+        },[])
+
+
+        return(
+            <div className="container-list">
+                <h2>Productos en Oferta</h2>
+                <div className="container-cards">
+                    
+                    {products.map((product) =>{
+                        const {id} = product
+                        return (
+                           <Card data={product} key={id}/> 
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    )
+        )
 }
 export default ItemListContainer;
